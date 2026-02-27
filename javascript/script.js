2b1a4b5279b68e6decd9b8b81af38b55a2c53bd8
Vaishnavi <Vyshnavireddy590@gmail.com>
2026-02-27 16:05:14 +0530

    JavaScript has done

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+// console.log("My cutest fellow piee..");
+// //Reintialize and redeclare also
+// var a;//global scope
+// a=10;
+// var a;
+// a=20;
+// //Reintialize
+// let b=10; //let=block scope
+// //cant redeclare or reintialize
+// const c=30;//constant scope cant be undo
+// //cant redeclare too c=40;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+var num=10;
+var str="PUTTI";//`` " " ''
+var bool=true;
+var un;
+var nu=null;
+var big=12345678n;
+var symbol=Symbol('li');
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof un);
+console.log(typeof nu);
+console.log(typeof big);
+console.log(typeof symbol);
+var arr=[1,2,3,4,1]
+console.log(typeof arr);
+var Obj=
+{
+    name:"Vaishu",Dept:`ds`
+}
+console.log(Obj);
+console.log(typeof Obj);
+//arithmetic operator
+var a=10;
+var b="2";
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+console.log(a*b);
+console.log(a**b)
+//==value
+//===data type
+
+
+
+
+
